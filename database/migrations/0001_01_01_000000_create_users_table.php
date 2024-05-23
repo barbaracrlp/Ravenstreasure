71b5a7bd8f82cb54e850861166cb819acb3d30b5
barbaracrlp <barbaracrlp99@gmail.com>
2024-05-23 20:47:40 +0200

    Primeros modelos se crean

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname1');
+            $table->string('surname2');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->boolean('log_in');
+            $table->boolean('is_designer')->default(false);
+
             $table->timestamps();
         });
 
