309fa9260cf83707e27f39b35ac333bf2125b212
barbaracrlp <barbaracrlp99@gmail.com>
2024-05-28 22:19:31 +0200

    Se añadn cambios de estilo y fotos,nuevo nav.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,14 @@
-
-  <nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src='{{asset('images/iconoRaven.png')}}'alt="" width="30" height="24" class="d-inline-block align-text-top">
-        Raven's Treasure
-      </a>
-      <a  class="navbar-brand" href="{{ route('home') }}">Home</a>
-      <a class="navbar-brand"  href="{{ route('designs.index') }}">Designs</a>
+        <a class="navbar-brand" href="#">
+            <img src='{{ asset('images/iconoRaven.png') }}'alt="" width="30" height="24"
+                class="d-inline-block align-text-top">
+            Raven's Treasure
+        </a>
+        <a class="navbar-brand" href="{{ route('home') }}">Home</a>
+        <a class="navbar-brand" href="{{ route('designs.index') }}">Designs</a>
+        <a class="navbar-brand" href="/admin">
+            Log in
+              </a>
     </div>
-  </nav>
+</nav>
