b23376f565ad0267bab636e5a6914735f32f029d
barbaracrlp <barbaracrlp99@gmail.com>
2024-05-29 11:29:48 +0200

    Seeder Usuario Admin se crea el seeder de usuario admin para poder acceder al panel de gestion

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'surname1' => 'admin',
+            'surname2' => 'admin',
+            'username' => 'Admin',
+            'email' => 'admin@admin.net',
+            'password' => Hash::make('admin123'), // Hashear la contraseña
+            'phone' => '1234567890',
+            'log_in' => true,
+            'is_designer' => true,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
