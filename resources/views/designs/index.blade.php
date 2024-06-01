7b62b08cf5715320a85a12fe1c16488c57f1ba70
barbaracrlp <barbaracrlp99@gmail.com>
2024-06-01 14:44:15 +0200

    Falta volver a cambiar las migraciones

--- resources/views/designs/index.blade.php
+++ resources/views/designs/index.blade.php
@@ -2,28 +2,22 @@
 @extends('template')
 
 @section('content')
-
-    <div class="container">
-        <div class="row">
-            @foreach ($designs as $design)
-            <div class="col-md-4">
-                    <div class="card mb-4" style="width: 18rem;">
-                        <img src="{{ Storage::url($design->file_path) }}" class="card-img-top" alt="{{ $design->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $design->name }}</h5>
-                            <h6 class="card-text">{{ $design->price }} €</h6>
-                            <p class="card-text"><b>Type:</b> {{ $design->type->name ?? 'N/A' }}</p>
-                            <p class="card-text"><b>Collection:</b> {{ $design->collection->name ?? 'N/A' }}</p>
-                            <p class="card-text">
-                                @foreach ($design->categories as $category)
-                                    {{ $category->name }}@if (!$loop->last), @endif
-                                @endforeach
-                            </p>
-                            <a href="{{ route('designs.show', ['design' => $design->id]) }}" class="btn btn-success">View</a>
-                        </div>
+<div class="container">
+    <div class="row">
+        @foreach ($designs as $design)
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <div class="position-relative">
+                    <img src="{{ Storage::url($design->file_path) }}" class="card-img-top" alt="{{ $design->name }}">
+                    <div class="card-img-overlay d-flex flex-column align-items-end" style="top: 0; right: 0;">
+                        <h4 class="card-title text-white mb-0" style="background-color: rgba(128, 128, 128, 0.5);padding:5px;">{{ $design->name }}</h4>
+                        <a href="{{ route('designs.show', ['design' => $design->id]) }}" class="buton-view btn btn-sm" style="background-color: #ccccb3; color: #000;">View</a>
                     </div>
                 </div>
-            @endforeach
+            </div>
         </div>
+        @endforeach
     </div>
+</div>
 @endsection
+
